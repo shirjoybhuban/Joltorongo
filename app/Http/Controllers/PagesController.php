879567a418154014c0499aafc78cf14dfac460fd
HellBlazer616 <rifat109@gmail.com>
2019-01-14 20:58:35 +0600

    Created Custom Controller and set up the pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index() 
+    {
+        if(Auth::guest())
+        {
+            return view('master');
+        }
+        else
+        {
+            return view('home');
+        }
+    }
+
+    public function home()
+    {
+        return view('home');
+    }
+}
