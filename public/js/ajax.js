41a77b37e00f99ae19c61e54f5c656170a05d6fc
HellBlazer616 <rifat109@gmail.com>
2019-01-31 13:36:41 +0600

    Dynamic Fade

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,30 +1,54 @@
 $(document).ready(function() {
-    $("#browse").click(function() {
-        $("#content").load("/browse");
+    $("#browse").click(function() 
+    {
+        $("#content").fadeOut('slow',function()
+        {
+            $("#content").load("/browse").fadeIn(slow);
+
+        });
         console.log(this.index);
     });
 });
 $(document).ready(function() {
-    $("#album").click(function() {
-        $("#content").load("/album");
+    $("#album").click(function() 
+    {
+        $("#content").fadeOut('slow',function()
+        {
+            $("#content").load("/album").fadeIn('slow');
+
+        });
         console.log(this.index);
     });
 });
 $(document).ready(function() {
-    $("#artist").click(function() {
-        $("#content").load("/artist");
+    $("#artist").click(function() 
+    {
+        $("#content").fadeOut('slow',function()
+        {
+            $("#content").load("/artist").fadeIn('slow');
+
+        });
         console.log(this.index);
     });
 });
 $(document).ready(function() {
-    $("#playlist").click(function() {
-        $("#content").load("/playlist");
+    $("#playlist").click(function() 
+    {
+        $("#content").fadeOut('slow',function()
+        {
+            $("#content").load("/playlist").fadeIn('slow');
+
+        });
         console.log(this.index);
     });
 });
 $(document).ready(function() {
-    $("#search").click(function() {
-        $("#content").load("/search");
+    $("#search").click(function() 
+    {
+        $("#content").fadeOut('slow',function()
+        {
+            $("#content").load("/search").fadeIn('slow');
+        });
         console.log(this.index);
     });
 });
