f10289105a322c9ee04cdd1d43984fe372324712
HellBlazer616 <rifat109@gmail.com>
2019-03-26 20:56:39 +0600

    Minor Indentation

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,84 +1,65 @@
 history.pushState(null, null, "/home");
-$(document).ready(function () {
-    $("#browse").click(function () {
-
+$(document).ready(function() {
+    $("#browse").click(function() {
         history.pushState(null, null, "/browse");
-        $("#dynamic")
-            .load("/browse");
-
+        $("#dynamic").load("/browse");
     });
 });
-$(document).ready(function () {
-    $("#album").click(function () {
-
+$(document).ready(function() {
+    $("#album").click(function() {
         history.pushState(null, null, "/album");
 
-        $("#dynamic")
-            .load("/album");
-
-
+        $("#dynamic").load("/album");
     });
 });
-$(document).ready(function () {
-    $(".sidebar-header").click(function () {
+$(document).ready(function() {
+    $(".sidebar-header").click(function() {
         history.pushState(null, null, "/home");
         $("#dynamic").load("/home #content .middle section");
     });
 });
-$(document).ready(function () {
-    $("#artist").click(function () {
-
+$(document).ready(function() {
+    $("#artist").click(function() {
         history.pushState(null, null, "/artist");
 
-        $("#dynamic")
-            .load("/artist");
-
+        $("#dynamic").load("/artist");
     });
 });
-$(document).ready(function () {
-    $("#playlist").click(function () {
-
+$(document).ready(function() {
+    $("#playlist").click(function() {
         history.pushState(null, null, "/playlist");
 
-        $("#dynamic")
-            .load("/playlist");
-
+        $("#dynamic").load("/playlist");
     });
 });
-$(document).ready(function () {
-    $("#search").on("click", function () {
-
+$(document).ready(function() {
+    $("#search").on("click", function() {
         history.pushState(null, null, "/index");
 
-        $("#dynamic")
-            .load("/index");
-
+        $("#dynamic").load("/index");
     });
 });
 
-$(document).on("click", "#artistPage li", function () {
+$(document).on("click", "#artistPage li", function() {
     history.pushState(null, null, "/" + this.id);
     $("#dynamic").load("/" + this.id);
 });
-$(document).on("click", "#content td", function () {
-    history.pushState(null, null, "/" + this.id);
-    $("#dynamic").load("/" + this.id);
-});
-
-$(document).on("click", "#albumPage li", function () {
+$(document).on("click", "#content td", function() {
     history.pushState(null, null, "/" + this.id);
     $("#dynamic").load("/" + this.id);
 });
 
-$(window).bind("popstate", function () {
+$(document).on("click", "#albumPage li", function() {
+    history.pushState(null, null, "/" + this.id);
+    $("#dynamic").load("/" + this.id);
+});
+
+$(window).bind("popstate", function() {
     link = location.pathname.replace(/^.*[\\/]/, "");
 
     if (link.valueOf() == "home")
         $("#dynamic").load("/home #content .middle section");
     else {
-
-        $("#dynamic")
-            .load(link);
-
+        $("#dynamic").load(link);
     }
 });
